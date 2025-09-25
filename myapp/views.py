380fc41de0705a85cad7e5f487a691e3e721c5f4
Williams Limonchi <wlimonchi@igp.gob.pe>
2025-09-25 17:25:28 -0500

    Params

--- myapp/views.py
+++ myapp/views.py
@@ -2,9 +2,16 @@ from django.http import HttpResponse
 from django.template import loader
 
 # Create your views here.
-def hello(request):
+
+def index(request):
     return HttpResponse(
-        "<h1>Hello ABS</h1>"
+        "<h1>INDEX PAGE</h1>"
+        )
+
+def hello(request, username):
+    print(username)
+    return HttpResponse(
+        "<h1>Hello %s</h1>" %username
         )
 
 def about (request):
